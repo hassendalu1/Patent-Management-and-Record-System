6e85d22ab5313191b08aada4af7d859040d9ad89
Eyuelzelalem <70477341+Eyuelzelalem@users.noreply.github.com>
2026-06-24 18:45:36 +0300

    Added Inpatient and Outpatient classes

--- src/InPatient.java
+++ src/InPatient.java
@@ -1,2 +1,22 @@
 public class InPatient {
 }
+the code; public class InPatient extends Patient {
+    private int daysAdmitted;
+    private double dailyRoomRate;
+}
+    public InPatient(String patientId, String name, int age, double baseFee, int daysAdmitted, double dailyRoomRate) {
+        super(patientId, name, age, baseFee); 
+        this.daysAdmitted = daysAdmitted;
+        this.dailyRoomRate = dailyRoomRate;
+    }
+
+    @Override
+    public double calculateBill() {
+        return baseFee + (daysAdmitted * dailyRoomRate);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Type: InPatient | Total Bill: $" + calculateBill();
+    }
+}
--- src/OutPatient.java
+++ src/OutPatient.java
@@ -1,2 +1,20 @@
 public class OutPatient {
 }
+the code: public class OutPatient extends Patient {
+    private double consultationFee;
+
+    public OutPatient(String patientId, String name, int age, double baseFee, double consultationFee) {
+        super(patientId, name, age, baseFee);
+        this.consultationFee = consultationFee;
+    }
+
+    @Override
+    public double calculateBill() {
+        return baseFee + consultationFee;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Type: OutPatient | Total Bill: $" + calculateBill();
+    }
+}
